25129721bbd9624ed2c4277dc983158c42328050
Giorgio Borelli <casaborelli@gmail.com>
2014-10-07 11:35:37 +0200

    add uninstall step

--- src/test/localutility/uninstall.py
+++ src/test/localutility/uninstall.py
@@ -3,7 +3,6 @@ from .component import ITestUtility
 
 def uninistall_utility(site):
     sm = site.getSiteManager()
-
     util = sm.queryUtility(ITestUtility)
     if util:
         sm.unregisterUtility(provided=ITestUtility)
@@ -14,6 +13,18 @@ def uninistall_utility(site):
         if provided.get(ITestUtility):
             del provided[ITestUtility]
         sm.utilities._provided = provided
+
+        subscribers = sm.utilities._subscribers
+        if subscribers[0].get(ITestUtility):
+            del subscribers[0][ITestUtility]
+        sm.utilities._subscribers = subscribers
+
+        # regsistrations = sm._utility_registrations
+        # _utility_registrations
+        # _reconstructor
+        # utilities
+
+    # import pdb; pdb.set_trace()
 
 
 def uninstall(context):
